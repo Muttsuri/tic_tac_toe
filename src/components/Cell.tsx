6646eb3bee2202b6092c085f5f6a201f2492cb9d
Muttsuri <Muttsuri@outlook.com>
2021-07-15 19:03:22 +0100

    Fix row size filckering on first slection and increase font size

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -10,7 +10,10 @@ function Cell({
   return (
     <div className="cell" onClick={onClick}>
       {/* Interestingly null doesn't get written, cool */}
-      <p>{value}</p>
+      <p>{value ? value : "\0"}</p> 
+      {/* The "\0" exists for the purpuse of having the P with soemthing
+          so that the grid doesn't flicker in size when the first cell in
+          a row is selected */}
     </div>
   );
 }
